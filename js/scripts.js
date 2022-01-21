3404595bfbe963182d0fda7334c062ac0f057404
jakechaley <jakechaley@gmail.com>
2022-01-21 09:12:27 -0800

    test one passes in code

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+// BUSINESS LOGIC
+
+function numberPrinter(number) {
+  let numberList = [];
+  for (let currentNumber = 1; currentNumber <= number; currentNumber ++) {
+    console.log(currentNumber);
+    numberList.push(currentNumber);
+  };
+  return numberList;
+};
+
+
+// UI LOGIC
+
+$(document).ready(function () {
+  $('form#input').submit(function (event) {
+    event.preventDefault();
+    const number = $('#number').val();
+    let result = numberPrinter(number)
+    $('#results').empty();
+    result.forEach(function(number) {
+      $('#results').append('<p>' + number + '</p>');
+      // console.log(number)
+
+    });
+  });
+});
